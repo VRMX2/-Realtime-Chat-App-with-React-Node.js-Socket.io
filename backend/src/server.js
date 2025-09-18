c6b48ee4f27f94f012dae606443640bd9cf3c4e5
VRMX2 <lehcengrissi@gmail.com>
2025-09-18 14:01:12 -0700

    new commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import 'dotenv/config';
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import authMessages from './routes/message.route.js';
 
 const app = express();
 
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", authMessages);
+
+
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+	app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 app.listen(port, () => console.log(`server running on the port ${port}`));
