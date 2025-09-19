81a270cd7fe27b723a3ada44b8054ea8617d84be
VRMX2 <lehcengrissi@gmail.com>
2025-09-19 13:24:10 -0700

    new commit

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,4 +1,35 @@
-function ChatPage(){
-    console.log("hello word");
+import { useChatStore } from "../store/useChatStore";
+
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import ProfileHeader from "../components/ProfileHeader";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatsList from "../components/ChatsList";
+import ContactList from "../components/ContactList";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
+
+function ChatPage() {
+  const { activeTab, selectedUser } = useChatStore();
+
+  return (
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* LEFT SIDE */}
+        <div className="flex flex-col w-80 bg-slate-800/50 backdrop-blur-sm">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+
+          <div className="flex-1 p-4 space-y-2 overflow-y-auto">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="flex flex-col flex-1 bg-slate-900/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+        </div>
+      </BorderAnimatedContainer>
+    </div>
+  );
 }
 export default ChatPage;
